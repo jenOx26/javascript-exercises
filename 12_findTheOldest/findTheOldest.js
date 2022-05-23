1d5033634ad091749edb9496d85b665fe14edcf0
Jen Ox <jnochsner26@gmail.com>
2022-05-23 13:16:07 -0600

    Use sort method, only passes first test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people = people.sort(function(a, b) {
+        if (people.yearOfDeath === undefined) {
+            const lastPerson = 2022 - a.yearOfBirth;
+            const nextPerson = 2022 - b.yearOfBirth;
+        } else {
+            const lastPerson = a.yearOfDeath - a.yearOfBirth;
+            const nextPerson = b.yearOfDeath - b.yearOfBirth;
+            return lastPerson > nextPerson ? -1 : 1;
+        }
+    }) 
+    return people[0];
 };
 
 // Do not edit below this line
